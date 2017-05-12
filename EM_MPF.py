corrected sparisty computation

EM_MPF.py
@@ -253,7 +253,7 @@ def em_mpf(hidden_units,learning_rate, epsilon, epoch = 400,  decay =0.0001,  ba
     np.save(saveloss, mean_epoch_error)
 
     savesparisty = path  + '/sparsity_params_' + str(hidden_units) + '.npy'
-    np.save(savesparisty, savesparisty)
+    np.save(savesparisty, sparsity_parameter)
 
     savesquaredweight = path  + '/squared_weight_' + str(hidden_units) + '.npy'
     np.save(savesquaredweight, squared_weights)

plotloss.py
@@ -1,30 +1,72 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#
+# path_1 = '../DBN_results/loss/loss_100.npy'
+# path_2 = '../DBN_results/loss/loss_196.npy'
+# path_3 = '../DBN_results/loss/loss_400.npy'
+#
+# path_4 = '../DBN_results/loss/loss.eps'
+#
+#
+# loss_1 = np.load(path_1)
+# loss_2 = np.load(path_2)
+# loss_3 = np.load(path_3)
+#
+# print(loss_1.shape)
+#
+# x = np.arange(len(loss_1))
+#
+# plt.plot(x, loss_1, 'r--')
+# plt.plot(x, loss_2, 'm-.')
+# plt.plot(x, loss_3, 'b')
+# plt.xlabel('Number of epoches')
+# plt.ylabel('Training Loss')
+# plt.axis([0, 400, 2, 8])
+# plt.legend([ 'VPF(100)', 'VPF(196)', 'VPF(400)'], loc='best')
+# #plt.title('Adam Training Loss')
+# plt.grid(False)
+# #plt.show()
+# plt.savefig(path_4)
 
-path_1 = '../DBN_results/loss/loss_100.npy'
-path_2 = '../DBN_results/loss/loss_196.npy'
-path_3 = '../DBN_results/loss/loss_400.npy'
-
-path_4 = '../DBN_results/loss/loss.eps'
 
 
-loss_1 = np.load(path_1)
-loss_2 = np.load(path_2)
-loss_3 = np.load(path_3)
+path_4 = '../DBN_results/sparsity/squared_weight_196.npy'
+path_5 = '../DBN_results/sparsity/sparsity_params_196.npy'
 
-print(loss_1.shape)
+squaredw = np.load(path_4)
+sparsity = np.load(path_5)
 
-x = np.arange(len(loss_1))
+print(squaredw.shape)
+print(sparsity.shape)
 
-plt.plot(x, loss_1, 'r--')
-plt.plot(x, loss_2, 'm-.')
-plt.plot(x, loss_3, 'b')
-plt.xlabel('Number of epoches')
-plt.ylabel('Training Loss')
-plt.axis([0, 400, 2, 8])
-plt.legend([ 'VPF(100)', 'VPF(196)', 'VPF(400)'], loc='best')
-#plt.title('Adam Training Loss')
-plt.grid(False)
-#plt.show()
-plt.savefig(path_4)
+x = np.arange(len(squaredw))
+
+plt.plot(x, squaredw, 'r--')
+# plt.plot(x, sparsity, 'm')
+# plt.xlabel('Number of epoches')
+# plt.ylabel('Training Loss')
+# plt.axis([0, 400, 2, 8])
+# plt.legend([ 'VPF(100)', 'VPF(196)', 'VPF(400)'], loc='best')
+# #plt.title('Adam Training Loss')
+# plt.grid(False)
+plt.show()
+#plt.savefig(path_4)
+
+
+# fig, ax1 = plt.subplots()
+# ax1.plot(x, squaredw, 'r--')
+# ax1.set_xlabel('Number of epoches')
+# # Make the y-axis label, ticks and tick labels match the line color.
+# ax1.set_ylabel('Squared weights W^2', color='b')
+# ax1.tick_params('y', colors='b')
+#
+# ax2 = ax1.twinx()
+# ax2.plot(x, sparsity, 'r')
+# ax2.set_ylabel('Weight sparsity p', color='r')
+# ax2.tick_params('y', colors='r')
+#
+# fig.tight_layout()
+# plt.show()
+
+

weight_visualize.py
@@ -21,7 +21,7 @@ print(W.shape)
 # f1 = np.asarray(W[:,90])
 # f1 = f1.reshape((28, 28))
 # print(f1.shape)
-
+#
 # image = Image.fromarray(
 #         tile_raster_images( X=W.T,
 #                         img_shape=(28, 28),
@@ -35,8 +35,8 @@ print(W.shape)
 # filter = [0, 40, 75, 124, 182, 263,
 #           90, 330, 83, 389, 225, 270,
 #           12, 120, 310, 240]
-
-
+#
+#
 # fig, axes = plt.subplots(nrows=4, ncols=4)
 # cmap = matplotlib.cm.plasma
 # for i in range(16):
@@ -52,45 +52,46 @@ print(W.shape)
 
 
 #############################  Visualize the activations / sparsity ##############
-# data = load_mnist()
-#
-# activations = sigmoid(np.dot(data[:10000], W) + b[W.shape[0]:])
-#
-# activations = np.mean(activations, axis = 0)
-# saveName1 = '../mpf_results/400/activations.eps'
-# fig1 = plt.figure()
-# # ax1 = fig1.add_subplot(111)
-# # ax1.set_title('Mean activations')
-# # plt.imshow( np.asarray(activations),aspect = 'auto')
-# # plt.colorbar()
-# plt.hist(activations, bins=20, color='b')
-# #plt.title('Mean activations of 400 hidden units')
-# plt.xlabel('Mean activation')
-# plt.ylabel('Number of hidden units')
-# #gplt.axis([0, 0.6, 0, 60])
-# fig1.savefig(saveName1)
-#
-# print(np.mean(activations))
+data = load_mnist()
+
+activations = sigmoid(np.dot(data[:10000], W) + b[W.shape[0]:])
+
+activations = np.mean(activations, axis = 0)
+saveName1 = '../mpf_results/400/activations.eps'
+fig1 = plt.figure()
+# ax1 = fig1.add_subplot(111)
+# ax1.set_title('Mean activations')
+# plt.imshow( np.asarray(activations),aspect = 'auto')
+# plt.colorbar()
+plt.hist(activations, bins=20, color='b')
+#plt.title('Mean activations of 400 hidden units')
+plt.xlabel('Mean activation')
+plt.ylabel('Number of hidden units')
+plt.axis([-0.01, 0.61, 0, 60])
+#plt.xlim(0, 0.6)
+fig1.savefig(saveName1)
+
+print(np.mean(activations))
 
 
 ###### visualize the weight with tile_rater_image ####
-W1 = []
-for i in range(25):
-    j = np.random.randint(low=0, high=W.shape[1])
-    W1.append(W[:,j])
-
-
-W1 = np.asarray(W1)
-# print(W1.shape)
-# W1 = W[:,:100].T
-
-visible_units = 784
-
-tile_shape = (5, 5)
-image = Image.fromarray(
-    tile_raster_images(X=W1,
-                    img_shape=(28, 28),
-                    tile_shape=tile_shape,
-                    tile_spacing=(1, 1)
-                ))
-image.save(saveName)
+# W1 = []
+# for i in range(25):
+#     j = np.random.randint(low=0, high=W.shape[1])
+#     W1.append(W[:,j])
+#
+#
+# W1 = np.asarray(W1)
+# # print(W1.shape)
+# # W1 = W[:,:100].T
+#
+# visible_units = 784
+#
+# tile_shape = (5, 5)
+# image = Image.fromarray(
+#     tile_raster_images(X=W1,
+#                     img_shape=(28, 28),
+#                     tile_shape=tile_shape,
+#                     tile_spacing=(1, 1)
+#                 ))
+# image.save(saveName)