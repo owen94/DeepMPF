7acd7e66b7279c8a6b133ca8a055efb32b52f173
owen94 <lcowen.hn@hotmail.com>
2017-05-12 09:43:17 +0800

    generate samples

--- sample_dbm.py
+++ sample_dbm.py
@@ -6,35 +6,48 @@ from utils_mpf import *
 from PIL import Image
 from DBM import *
 
-path1 = '../Thea_mpf/DBM/DBM_400_196_64/dbm_299.pkl'
-savepath1 = '../Thea_mpf/DBM/DBM_400_196_64'
-path2 = '../Thea_mpf/DBM/DBM_196_196_64/dbm_299.pkl'
-savepath2 = '../Thea_mpf/DBM/DBM_196_196_64'
+# path1 = '../Thea_mpf/DBM/DBM_400_196_64/dbm_299.pkl'
+# savepath1 = '../Thea_mpf/DBM/DBM_400_196_64'
+# path2 = '../Thea_mpf/DBM/DBM_196_196_64/dbm_299.pkl'
+# savepath2 = '../Thea_mpf/DBM/DBM_196_196_64'
+#
+# path3 = '../Thea_mpf/DBM/DBM_1000_400_64/dbm_99.pkl'
+# savepath3 = '../Thea_mpf/DBM/DBM_1000_400_64'
+#
+# path4 = '../Thea_mpf/DBM/DBM_196_64/dbm_99.pkl'
+# savepath4 = '../Thea_mpf/DBM/DBM_196_64'
+#
+# hidden_list = [784, 196, 64]
+# dbm1 = load(path1)
+# dbm2 = load(path2)
+# dbm3 = load(path3)
+# dbm4 = load(path4)
 
-path3 = '../Thea_mpf/DBM/DBM_1000_400_64/dbm_99.pkl'
-savepath3 = '../Thea_mpf/DBM/DBM_1000_400_64'
 
-path4 = '../Thea_mpf/DBM/DBM_196_64/dbm_99.pkl'
-savepath4 = '../Thea_mpf/DBM/DBM_196_64'
+# path_w = '../DBM_results/Samples/196_64/weight_599.npy'
+# path_b = '../DBM_results/Samples/196_64/bias_599.npy'
+# savepath1 = '../DBM_results/Samples/196_64'
+
+path_w = '../DBM_results/Samples/196_196_64/weight_599.npy'
+path_b = '../DBM_results/Samples/196_196_64/bias_599.npy'
+savepath1 = '../DBM_results/Samples/196_196_64'
 
 
+W = np.load(path_w)
+b = np.load(path_b)
+hidden_list = [784,196,196,64]
 
-hidden_list = [784, 196, 64]
-dbm1 = load(path1)
-dbm2 = load(path2)
-dbm3 = load(path3)
-dbm4 = load(path4)
-num_rbm = 3
+num_rbm = len(hidden_list) -1
 
-W = []
-b = []
-for i in range(num_rbm):
-    W.append(dbm4.W[i].get_value(borrow = True))
-    b.append(dbm4.b[i].get_value(borrow = True))
+# W = []
+# b = []
+# for i in range(num_rbm):
+#     W.append(dbm4.W[i].get_value(borrow = True))
+#     b.append(dbm4.b[i].get_value(borrow = True))
 
-n_chains = 20
+n_chains = 10
 n_samples = 10
-plot_every = 4
+plot_every = 5
 image_data = np.zeros(
     (29 * n_samples + 1, 29 * n_chains - 1), dtype='uint8'
 )
@@ -59,6 +72,24 @@ for idx in range(n_samples):
         v_samples = down_sample1
     print(' ... plotting sample ', idx)
 
+    #
+    # for j in range(plot_every):
+    #     for i in range(num_rbm):
+    #         vis_units = hidden_list[num_rbm-i - 1]
+    #         W_sample = W[num_rbm - i -1 ][:vis_units,vis_units:]
+    #         b_down = b[num_rbm - i -1 ][:vis_units]
+    #         downact1 = sigmoid(np.dot(v_samples,W_sample.T) + b_down )
+    #         down_sample1 = np.random.binomial(n=1, p= downact1)
+    #         v_samples = down_sample1
+    #     for i in range(num_rbm):
+    #         vis_units = hidden_list[i]
+    #         W_sample = W[i ][:vis_units,vis_units:]
+    #         b_up = b[i ][vis_units:]
+    #         upact1 = sigmoid(np.dot(v_samples,W_sample)+b_up)
+    #         v_samples = np.random.binomial(n=1,p=upact1)
+    # print(' ... plotting sample ', idx)
+
+
     image_data[29 * idx:29 * idx + 28, :] = tile_raster_images(
         X= downact1,
         img_shape=(28, 28),
@@ -67,4 +98,4 @@ for idx in range(n_samples):
     )
 
 image = Image.fromarray(image_data)
-image.save(savepath3 + '/samples.png')
+image.save(savepath1 + '/samples.eps')
--- weight_visualize.py
+++ weight_visualize.py
@@ -10,17 +10,17 @@ from PIL import Image
 from utils_mpf import tile_raster_images, load_mnist, sigmoid
 
 
-weight1 = '../mpf_results/196/weights_499.npy'
-bias1 = '../mpf_results/196/bias_499.npy'
-saveName = '../mpf_results/196/196_filter.eps'
+weight1 = '../mpf_results/400/weights_499.npy'
+bias1 = '../mpf_results/400/bias_499.npy'
+saveName = '../mpf_results/400/400_filter.eps'
 
 W = np.load(weight1)
 b = np.load(bias1)
 print(W.shape)
 
-f1 = np.asarray(W[:,90])
-f1 = f1.reshape((28, 28))
-print(f1.shape)
+# f1 = np.asarray(W[:,90])
+# f1 = f1.reshape((28, 28))
+# print(f1.shape)
 
 # image = Image.fromarray(
 #         tile_raster_images( X=W.T,
@@ -37,17 +37,17 @@ print(f1.shape)
 #           12, 120, 310, 240]
 
 
-fig, axes = plt.subplots(nrows=4, ncols=4)
-cmap = matplotlib.cm.gray
-for i in range(16):
-    j = np.random.randint(low=0, high=W.shape[1])
-    im = axes.flat[i].imshow(np.asarray(W[:,j]).reshape((28,28)), cmap=cmap, vmin=-0.5, vmax=0.5)
-    axes.flat[i].axis('off')
-
-cax,kw = matplotlib.colorbar.make_axes([ax for ax in axes.flat])
-plt.colorbar(im, cax=cax, **kw)
-#plt.title('Filters')
-fig.savefig(saveName)
+# fig, axes = plt.subplots(nrows=4, ncols=4)
+# cmap = matplotlib.cm.plasma
+# for i in range(16):
+#     j = np.random.randint(low=0, high=W.shape[1])
+#     im = axes.flat[i].imshow(np.asarray(W[:,j]).reshape((28,28)), cmap=cmap, vmin=-0.5, vmax=0.5)
+#     axes.flat[i].axis('off')
+#
+# cax,kw = matplotlib.colorbar.make_axes([ax for ax in axes.flat])
+# plt.colorbar(im, cax=cax, **kw)
+# #plt.title('Filters')
+# fig.savefig(saveName)
 
 
 
@@ -74,3 +74,23 @@ fig.savefig(saveName)
 
 
 ###### visualize the weight with tile_rater_image ####
+W1 = []
+for i in range(25):
+    j = np.random.randint(low=0, high=W.shape[1])
+    W1.append(W[:,j])
+
+
+W1 = np.asarray(W1)
+# print(W1.shape)
+# W1 = W[:,:100].T
+
+visible_units = 784
+
+tile_shape = (5, 5)
+image = Image.fromarray(
+    tile_raster_images(X=W1,
+                    img_shape=(28, 28),
+                    tile_shape=tile_shape,
+                    tile_spacing=(1, 1)
+                ))
+image.save(saveName)
