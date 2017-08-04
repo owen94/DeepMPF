bug in std computation

DBM.py
@@ -129,7 +129,7 @@ class DBM(object):
 
 
 
-    def get_cost_update(self,decay_list = [], learning_rate = 0.001):
+    def get_cost_update(self,decay_list = [], learning_rate = 0.0001):
         '''
         :param undirected_act: The input from the forward and backward pass
         :return: the cost function and the updates
@@ -177,7 +177,7 @@ class DBM(object):
         return cost, updates
 
 
-def train_dbm(hidden_list, decay, lr, undirected = False,  batch_sz = 40, epoch = 400):
+def train_dbm(hidden_list, decay, lr, undirected = False,  batch_sz = 40, epoch = 200):
 
     data = load_mnist()
 
@@ -351,7 +351,7 @@ def train_dbm(hidden_list, decay, lr, undirected = False,  batch_sz = 40, epoch 
             image = Image.fromarray(image_data)
             image.save(path + '/samples_' + str(n_epoch) + '.png')
 
-        if n_epoch % 5 == 0:
+        if n_epoch % 10 == 0:
             W = []
             b = []
             for i in range(num_rbm):
@@ -376,9 +376,10 @@ def train_dbm(hidden_list, decay, lr, undirected = False,  batch_sz = 40, epoch 
             #     feed_w = W[i][:feed_vis_units,feed_vis_units:]
             #     feed_b = b[i][feed_vis_units:]
             #     feed_data = sigmoid(np.dot(feed_data, feed_w) + feed_b)
+            error_bar_train_lld = []
+            error_bar_test_lld = []
 
-
-            for i in range(1):
+            for kk in range(10):
 
                 feed_samplor = get_samples(hidden_list=hidden_list, W=W, b=b)
                 feed_data = feed_samplor.get_mean_activation(input_data= training_data)
@@ -407,21 +408,20 @@ def train_dbm(hidden_list, decay, lr, undirected = False,  batch_sz = 40, epoch 
                 # compute the log-likelihood for the training data
                 epoch_train_lld = get_ll(x=train_data[:10000],
                                          gpu_parzen=gpu_parzen(mu=parzen_sample,sigma=0.2),batch_size=20)
-                train_mean_lld = np.mean(np.array(epoch_train_lld))
-                print(np.array(epoch_train_lld))
-                train_std_lld = np.std(np.array(epoch_train_lld))
-                print(train_lld)
-                train_lld += [train_mean_lld]
-                train_std += [train_std_lld]
-
+                error_bar_train_lld += [np.mean(np.array(epoch_train_lld))]
                 # comppute the log-likelihood for the test data
                 epoch_test_lld = get_ll(x=test_data, gpu_parzen=gpu_parzen(mu=parzen_sample,sigma=0.2),batch_size=10)
-                test_mean_lld = np.mean(np.array(epoch_test_lld))
-                test_std_lld = np.std(np.array(epoch_test_lld))
-                test_lld += [test_mean_lld]
-                test_std += [test_std_lld]
+                error_bar_test_lld +=  [np.mean(np.array(epoch_test_lld))]
 
-                print('The loglikehood in epoch {} is: train {}, test {}'.format(n_epoch, train_mean_lld, test_mean_lld))
+            print(error_bar_test_lld)
+
+            train_lld += [np.mean(np.array(error_bar_train_lld))]
+            train_std += [np.std(np.array(error_bar_train_lld))]
+
+            test_lld += [np.mean(np.array(error_bar_test_lld))]
+            test_std += [np.std(np.array(error_bar_test_lld))]
+
+            print('The loglikehood in epoch {} is: train {}, test {}'.format(n_epoch,train_lld[-1], test_lld[-1]))
 
     path_1 = path + '/train_lld.npy'
     path_2 = path + '/train_std.npy'